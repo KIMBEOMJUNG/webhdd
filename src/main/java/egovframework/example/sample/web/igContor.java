7b66604cd84a5c1b607021e558f8e1ef768ca885
KIMBEOMJUNG <rla5451@naver.com>
2020-05-10 13:11:12 +0900

    ig

---
+++ src/main/java/egovframework/example/sample/web/igContor.java
@@ -0,0 +1,30 @@
+package egovframework.example.sample.web;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import egovframework.example.sample.service.impl.SampleDAO;
+
+@Controller
+public class igContor {
+	@Resource(name = "sampleDAO")
+	   private SampleDAO sampleDAO;
+	   @Resource(name= "multipartResolver")
+	   CommonsMultipartResolver multipartResolver;
+
+	   @RequestMapping(value="/ig/igfn.do")
+	   public String main(HttpServletRequest request, ModelMap model) throws Exception
+	   {
+	   /*List<?> list = (List<?>) sampleDAO.list("selecta");   //Sample_sql.xml 파일 안에 select id 와 일치시켜주기!
+	   model.addAttribute("resultList", list);*/
+	      return "ig/igfn";
+
+
+	   }
+
+}
